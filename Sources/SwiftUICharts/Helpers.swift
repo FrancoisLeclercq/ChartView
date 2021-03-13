22ffa954b09eb457eaaf53ef30585c38365be07c
François Leclercq <44198902+FrancoisLeclercq@users.noreply.github.com>
2021-03-13 17:19:13 +0100

    Update Helpers.swift

--- Sources/SwiftUICharts/Helpers.swift
+++ Sources/SwiftUICharts/Helpers.swift
@@ -131,6 +131,7 @@ public struct Styles {
 }
 
 public struct ChartForm {
+    public var chartHeight: Int
     #if os(watchOS)
     public static let small = CGSize(width:120, height:90)
     public static let medium = CGSize(width:120, height:160)
@@ -139,8 +140,8 @@ public struct ChartForm {
     public static let detail = CGSize(width:180, height:160)
     #else
     public static let small = CGSize(width:180, height:120)
-    public static let medium = CGSize(width:180, height:240)
-    public static let large = CGSize(width:360, height:120)
+    public static let medium = CGSize(width: (UIScreen.main.bounds.width / 2) - 15, height: chartHeight)
+    public static let large = CGSize(width: UIScreen.main.bounds.width - 20, height: chartHeight)
     public static let extraLarge = CGSize(width:360, height:240)
     public static let detail = CGSize(width:180, height:120)
     #endif
