a7a5e8fb1dc2a0da946ff7ceee321ca0a5df1dba
François Leclercq <44198902+FrancoisLeclercq@users.noreply.github.com>
2021-03-14 00:32:09 +0100

    Update BarChartView.swift

--- Sources/SwiftUICharts/BarChart/BarChartView.swift
+++ Sources/SwiftUICharts/BarChart/BarChartView.swift
@@ -33,7 +33,7 @@ public struct BarChartView : View {
     var isFullWidth:Bool {
         return self.formSize == ChartForm.large
     }
-    public init(data:ChartData, title: String, legend: String? = nil, style: ChartStyle = Styles.barChartStyleOrangeLight, darkModeStyle: ChartStyle = Styles.barChartStyleOrangeDark, form: CGSize? = ChartForm.medium, dropShadow: Bool? = true, cornerImage:Image? = Image(systemName: "waveform.path.ecg"), valueSpecifier: String? = "%d"){
+    public init(data:ChartData, title: String, legend: String? = nil, style: ChartStyle = Styles.barChartStyleOrangeLight, darkModeStyle: ChartStyle = Styles.barChartStyleOrangeDark, form: CGSize? = ChartForm.medium, dropShadow: Bool? = true, cornerImage:Image? = Image(systemName: "waveform.path.ecg"), valueSpecifier: String? = "%.0f"){
         self.data = data
         self.title = title
         self.legend = legend
